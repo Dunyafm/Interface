99a5f8414c12c796de92c99ae69684af6719633b
unknown <Dunyafm@code.edu.az>
2021-12-17 12:08:19 -0800

    Interface1

---
+++ Interface1/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{ 
+    public abstract class animal
+    {
+        static void Main(string[] args)
+        {
+            animal animal = new animal();
+            animal myAnimal = animal;
+            animal myDog = new Dog();
+            animal myCat = new Cat();
+
+            myAnimal.animalSound();
+            myDog.animalSound();
+            myCat.animalSound();
+
+        }
+
+        private void animalSound()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AnimalSound()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+ 
---
+++ Interface1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Interface;
+using System;
+
+namespace Interface
+{
+    public class Program
+    {
+        public void AnimalSound()
+        {
+            Console.WriteLine("The animal makes a sound" );
+        }
+    }
+}
+ class Dog : animal 
+{
+    public void AnimalSound()
+    {
+        Console.WriteLine("The dog says : hauv hauv");
+    }
+}
+class Cat : animal
+{
+    public void AnimalSound()
+    {
+        Console.WriteLine("The cat says : mau mau");
+    }
+}
